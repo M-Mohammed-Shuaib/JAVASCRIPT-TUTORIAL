30a2580480acf8d2f490a736ce6597753879461d
M-Mohammed-Shuaib <shuaib0419@gmail.com>
2025-11-24 23:04:41 +0530

    done exercise 2

--- variables.js
+++ variables.js
@@ -4,3 +4,10 @@ var tax=20
 console.log(product);
 var totalPrice = price + tax;
 console.log(totalPrice);
+
+var FruitName ="apple"
+var count =5
+var price =10
+var total =count * price
+console.log(FruitName);
+console.log(total);
