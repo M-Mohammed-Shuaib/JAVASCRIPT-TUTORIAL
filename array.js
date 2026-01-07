65938dab6d2cc349c82777de9d0ea19ca547908e
M-Mohammed-Shuaib <shuaib0419@gmail.com>
2026-01-07 22:54:27 +0530

    learned some concepts in dom manioulation with tasks

---
+++ array.js
@@ -0,0 +1,8 @@
+var fruit =["Apple","Orange","Grape","Banana"]
+
+console.log(fruit.length)
+
+for (count =0; count<fruit.length; count=count+1)
+{
+    console.log(fruit[count])
+}
---
+++ random.js
@@ -0,0 +1,9 @@
+var a = Math.random()
+console.log(math.floor(a*10))
+
+var b = Math.floor(6.99)
+console.log(b)
+
+var a = Math.random()
+console.log(Math.floor(a*10)+1)
+
